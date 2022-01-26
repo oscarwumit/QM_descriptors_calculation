f7e6a674f506203d7efc17c0c7967da257b2def5
Chas <charlesjmcgill@gmail.com>
2022-01-26 10:53:00 -0500

    Fix relative directory references for xtb

--- lib/xtb_optimization.py
+++ lib/xtb_optimization.py
@@ -20,21 +20,21 @@ def xtb_optimization(folder, sdf, xtb_path, logger):
 
     try:
         xtb_command = os.path.join(xtb_path, 'xtb')
-        with open('{}_xtb_opt.log'.format(file_name), 'w') as out:
-            print(xtb_command, '{}.sdf'.format(file_name))
-            subprocess.call([xtb_command, '{}.sdf'.format(file_name), '-opt'],
+        with open(os.path.join('..','{}_xtb_opt.log'.format(file_name)), 'w') as out:
+            print(xtb_command, os.path.join('..','{}.sdf'.format(file_name)))
+            subprocess.call([xtb_command, os.path.join('..','{}.sdf'.format(file_name)), '-opt'],
                             stdout=out, stderr=out)
-            shutil.move('xtbopt.sdf', '{}_opt.sdf'.format(file_name))
-            os.remove('{}.sdf'.format(file_name))
+            shutil.move('xtbopt.sdf', os.path.join('..','{}_opt.sdf'.format(file_name)))
+            os.remove(os.path.join('..','{}.sdf'.format(file_name)))
 
-        with open(file_name + '_freq.log', 'w') as out:
-            subprocess.call([xtb_command, '{}_opt.sdf'.format(file_name), '-ohess'], stdout=out,
+        with open(os.path.join('..',file_name + '_freq.log'), 'w') as out:
+            subprocess.call([xtb_command, os.path.join('..','{}_opt.sdf'.format(file_name)), '-ohess'], stdout=out,
                             stderr=out)
 
             # os.remove('hessian')
             # os.remove('vibspectrum')
 
-        log = XtbLog('{}_freq.log'.format(file_name))
+        log = XtbLog(os.path.join('..','{}_freq.log'.format(file_name)))
     finally:
         os.chdir(pwd)
         shutil.rmtree(scratch_dir)
