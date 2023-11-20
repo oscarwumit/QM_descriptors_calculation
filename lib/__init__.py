78abceace09ad4ad9af44702d44a7e8f31bcb6fe
shihchengli <scli@mit.edu>
2023-11-20 17:53:46 -0500

    Support more QM descriptors to be parsed

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,7 @@
 from .dftscf import dft_scf
 from .file_parser import mol2xyz, xyz2com
-from .g16_log import G16Log, XtbLog
+from .g16_log import G16NBOQMData, XtbLog
 from .genConf import csearch
-from .grab_QM_descriptors import read_log
 from .utils import create_logger
 from .xtb_optimization import xtb_optimization
+from .json2csv import json2csv
